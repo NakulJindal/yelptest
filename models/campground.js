bcc11b52fbeca79d3e523d8cd2bb19eab70567e2
NakulJindal <jnannu1@gmail.com>
2024-04-14 17:10:21 +0530

    added cluster map

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Review = require("./review");
-const User = require("./user");
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
@@ -12,33 +11,44 @@ ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const opts = { toJSON: { virtuals: true }, strictPopulate: false };
+
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
   },
+  opts
+);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 70)}...</p>`;
 });
 
 CampgroundSchema.post("findOneAndDelete", async (camp) => {
